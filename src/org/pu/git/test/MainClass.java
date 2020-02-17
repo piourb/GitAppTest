e2df7185bd50c6e9bfe08336066356b6fdcd926c
markuswwwoppl <markuswww@gmail.com>
2020-02-17 13:17:38 +0100

    update note

--- src/org/pu/git/test/MainClass.java
+++ src/org/pu/git/test/MainClass.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MainClass {
 	
-	public FeatureBase tab[]= {new Feature001(),new Feature002(),new Feature003()};
+	public FeatureBase tab[]= {new Feature001(),new Feature002(),new Feature003(),new Feature003()};
 		
 	public static void main(String[] args) {
 		Arrays.asList(new MainClass().tab).stream().forEach(e->e.print());		
