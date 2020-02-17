d5dd357b003760b8e98f98fea7fa7550e7fb6142
markuswww <markuswww@op.pl>
2020-02-17 11:17:04 +0100

    added feature 001

---
+++ src/org/pu/git/test/Feature001.java
@@ -0,0 +1,9 @@
+package org.pu.git.test;
+
+public class Feature001 {
+	
+	public void print() {
+		System.out.println("print feature one");
+	}
+
+}
--- src/org/pu/git/test/MainClass.java
+++ src/org/pu/git/test/MainClass.java
@@ -2,6 +2,11 @@ package org.pu.git.test;
 
 public class MainClass {
 	
+	public Feature001 feature001=new Feature001();
 	
-
+	public static void main(String[] args) {
+		new MainClass().feature001.print();
+	}
+	
+	
 }
